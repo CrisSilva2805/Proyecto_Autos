eb0cb688a44eea2e03dc90e5dd53c34f3bc8adca
CrisSilva2805 <crissilvazamarripa9788@gmail.com>
2026-05-19 22:03:27 -0600

    Interfaz de Login

--- src/login/Login.java
+++ src/login/Login.java
@@ -5,12 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSeparator;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class Login extends JFrame {
 
@@ -18,6 +23,7 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
 
 	/**
 	 * Launch the application.
@@ -40,49 +46,105 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 768, 432);
+		setBounds(100, 100, 605, 490);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 0, 160));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("FORNI-CAR");
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Agency FB", Font.BOLD | Font.ITALIC, 55));
-		lblNewLabel.setBounds(271, 33, 232, 107);
-		contentPane.add(lblNewLabel);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(0, 0, 242, 514);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("Login");
-		lblNewLabel_1.setFont(new Font("Agency FB", Font.BOLD, 30));
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(346, 135, 104, 41);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/com/images/Car dealership selling cars_ Neural network AI generated royalty free stock photo.jpg")));
+		lblNewLabel_1.setBounds(0, 0, 242, 257);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("FORNI-CAR");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblNewLabel_2.setBounds(53, 268, 132, 32);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Drive Your Dream.");
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(63, 305, 122, 12);
+		panel.add(lblNewLabel_3);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setForeground(Color.WHITE);
+		separator_2.setBounds(42, 344, 150, 1);
+		panel.add(separator_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("Cristian Alejandro Silva Zamarripa");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setBounds(0, 365, 232, 12);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Angel Alejandro De Los Santos Lares");
+		lblNewLabel_4_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblNewLabel_4_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1.setBounds(0, 387, 232, 12);
+		panel.add(lblNewLabel_4_1);
+		
+		JLabel lblNewLabel_4_1_1 = new JLabel("Diego Alejandro Delgado De La Torre");
+		lblNewLabel_4_1_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblNewLabel_4_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1.setBounds(0, 409, 232, 12);
+		panel.add(lblNewLabel_4_1_1);
 		
 		textField = new JTextField();
-		textField.setBounds(319, 216, 163, 18);
+		textField.setBounds(301, 83, 233, 29);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("User:");
-		lblNewLabel_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1.setFont(new Font("Agency FB", Font.PLAIN, 24));
-		lblNewLabel_1_1.setBounds(253, 199, 56, 41);
-		contentPane.add(lblNewLabel_1_1);
+		JLabel lblNewLabel = new JLabel("USERNAME");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setBounds(301, 61, 68, 12);
+		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Password:");
-		lblNewLabel_1_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1_1.setFont(new Font("Agency FB", Font.PLAIN, 24));
-		lblNewLabel_1_1_1.setBounds(228, 239, 81, 41);
-		contentPane.add(lblNewLabel_1_1_1);
+		JSeparator separator = new JSeparator();
+		separator.setBounds(301, 140, 233, 2);
+		contentPane.add(separator);
+		
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPassword.setBounds(301, 172, 68, 12);
+		contentPane.add(lblPassword);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(319, 256, 163, 18);
+		passwordField.setBounds(301, 194, 233, 29);
 		contentPane.add(passwordField);
 		
-		JButton btnNewButton = new JButton("DONE");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnNewButton.setBounds(340, 315, 84, 20);
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(301, 259, 233, 2);
+		contentPane.add(separator_1);
+		
+		JLabel lblEmail = new JLabel("EMAIL");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblEmail.setBounds(301, 286, 68, 12);
+		contentPane.add(lblEmail);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(301, 308, 233, 29);
+		contentPane.add(passwordField_1);
+		
+		JButton btnNewButton = new JButton("SignUP");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBackground(new Color(120, 23, 5));
+		btnNewButton.setBounds(353, 382, 127, 20);
 		contentPane.add(btnNewButton);
 
 	}
